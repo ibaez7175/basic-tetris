4fc93bcba6d3490925574ebf73de141a7db900d0
idaliz <idaliz.baez@gmail.com>
2020-05-14 15:46:36 +0100

    Add app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
         'lawngreen'
     ]
 
-//The Tetrominos
+//The Tetrominos shapes
 const lTetromino = [
     [1,  width+1, width*2+1, 2],
     [width, width+1, width+2, width*2+2],
